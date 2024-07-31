a32b5620e8bd5c9dd0a6000a39bb15aa2aed312d
Yash Wadgaonkar <wadgaonkaryash782@gmail.com>
2024-07-31 19:05:16 +0530

    Basic setup done

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env",
+});
+
+connectDB();
+/*
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("Error", (error) => {
+            console.log("Error", error);
+            throw error;
+        });
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listing on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.error("Error: ", error);
+        throw err;
+    }
+})();
+*/
