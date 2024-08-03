93ddd079dccdf1809313595817cbf3d7b8af268a
Yash Wadgaonkar <wadgaonkaryash782@gmail.com>
2024-08-04 00:15:40 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -3,10 +3,16 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
 
-app.use(cors({ origin: process.evn.CORS_ORIGIN, credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(express.cookieParser());
+app.use(cookieParser());
 
+//Import routes
+import userRouter from "./routes/user.routes.js";
+
+//Route declaration
+app.use("/api/v1/users", userRouter);
 export default app;
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 } from "cloudinary";
+import { response } from "express";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECERT,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        // Else upload file on cloudinary
+        const response = await cloudinary.v2.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        });
+        //File uploaded sucessfully
+        console.log("File is uploaded on cloudinary", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+};
